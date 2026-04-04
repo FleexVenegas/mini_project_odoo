f575b31bf6f375ab3c3966eac3f3867faaa2cf36
FleexVenegas <diego.ven80@gmail.com>
2026-04-03 23:15:26 -0600

    .

--- addons/odoo_wp_sync/models/woo_instance_warehouse.py
+++ addons/odoo_wp_sync/models/woo_instance_warehouse.py
@@ -12,3 +12,15 @@ class WooInstanceWarehouse(models.Model):
         string="Almacén",
         help="Almacén por defecto para los movimientos de stock de los pedidos sincronizados",
     )
+
+    update_stock = fields.Boolean(
+        string="Actualizar Stock",
+        default=False,
+        help="Si está activo, se actualizará el stock en WooCommerce al sincronizar los pedidos",
+    )
+
+    update_order_status_wc = fields.Boolean(
+        string="Actualizar estado en WooCommerce",
+        default=False,
+        help="Si está activo, se actualizará el estado del pedido en WooCommerce al sincronizar (por ejemplo, a 'processing' o 'completed' según la configuración de estados)",
+    )
